d5e88a4fe6e5cd4ca95d8c494ac2b06be8a36ae2
Lorenzo Bandini <lboa@hotmail.it>
2025-06-05 19:02:16 +0200

    adding gas report config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,27 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomicfoundation/hardhat-ethers';
 import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.28',
+  solidity: {
+    version: '0.8.28',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   typechain: {
     target: 'ethers-v6',
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 20,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    rst: true,
   },
 };
 
